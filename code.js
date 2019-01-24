b033d6f39307b29b34be8ed39546ea81deba6eed
Christopher Magee <christophermagee10@gmail.com>
2019-01-24 23:14:55 +0000

    Algorithm Entirely finished

--- code.js
+++ code.js
@@ -1,32 +1,34 @@
 function calculate() {
-    //2D array of rates. first dimension is the lower limit of that bracket
-    //Second dimension is the rate
+    //2D array of rates.
+    //[minimum for that band, rate in band, max tax for that band]
     let rates = [
-        [11851, 0.2],
-        [46351, 0.4],
-        [150000, 0.45]
-        [0, 0]
+        [11850, 0.2, 6900],
+        [46350, 0.4, 41460],
+        [150000, 0.45, 0]
     ]
 
-    let rawIncome = document.getElementById("income").value;
     let tax = 0;
+    let rawIncome = document.getElementById("income").value;
     if (rawIncome < rates[0][0]) {
         document.getElementById("breakdown").innerHTML =
             "All of your income fell into the personal tax bracket. This means " +
             "that you do not owe any taxes and will keep 100% of your income";
     } else {
-        let i = 0;
+        let band = 0;
         do {
-            i++;
-            console.log(i);
+            band++;
+        } while ((band < 3) && (rawIncome > rates[band][0]));
+        band--;
+
+        console.log(band);
+        console.log(tax);
+
+        for (j = 0; j < band; j++) {
+            tax += rates[j][2];
         }
-        while ((i < 3) && (rawIncome > rates[i][0]));
-        for (j = 0; j < i; j++) {
-            tax += (rawIncome - rates[j][0]) * rates[j][1];
-        }
+        tax += (rawIncome - rates[band][0]) * rates[band][1];
+
     }
-    //Rounding because JS is strange with floating point numbers
-    tax = Math.round((tax + 0.00001) * 100) / 100;
     document.getElementById("result").innerHTML = ("Post Tax income: £" + (rawIncome - tax));
     console.log(tax);
 }
